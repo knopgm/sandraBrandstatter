d6b2159c58f12ef37bcb0af865d48deef0833034
knopgm <knop.gm@gmail.com>
2023-09-14 15:24:16 +0200

    feat: Create navigation buttons to look back and foward of all the projects images in the modal view

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,18 +8,26 @@ const openImgModal = document.getElementById("image");
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
+// Create an array of images for the circle motion on arrow buttons
+console.log({ openModal });
+const projectImages = [];
+
 // When the user clicks on the button(img), open the modal
 openModal.forEach((item) => {
-  console.log({ item });
+  const target = item.src;
+  if (target !== "") {
+    projectImages.push(target);
+  }
+
   item.addEventListener("click", (e) => {
     const target = e.target.src;
     console.log({ target });
-
     modal.style.display = "block";
     openImgModal.setAttribute("src", target);
   });
 });
 
+console.log({ projectImages });
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
@@ -31,3 +39,48 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+//Get the arrow buttons
+const arrows = document.getElementsByClassName("arrow");
+console.log({ arrows });
+
+const next = document.querySelectorAll("arrow right");
+console.log({ next });
+
+const previous = document.querySelectorAll("arrow left");
+console.log({ previous });
+
+// When the user clicks on the arrow buttons, the image will change
+[...arrows].forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const target = e.target;
+    console.log({ target });
+    const imageSrc = openImgModal.src;
+
+    let currentIndex = projectImages.indexOf(imageSrc);
+    console.log({ currentIndex });
+
+    if (target === arrow_right) {
+      console.log("right");
+      if (currentIndex === projectImages.length - 1) {
+        currentIndex = 0;
+      } else {
+        currentIndex = currentIndex + 1;
+      }
+
+      console.log({ currentIndex });
+      openImgModal.setAttribute("src", projectImages[currentIndex]);
+    }
+
+    if (target === arrow_left) {
+      console.log("left");
+      if (currentIndex === 0) {
+        currentIndex = projectImages.length - 1;
+      } else {
+        currentIndex = currentIndex - 1;
+      }
+      console.log({ currentIndex });
+      openImgModal.setAttribute("src", projectImages[currentIndex]);
+    }
+  });
+});
