a7981235cf5ce5537d44f1adaea77c2eb6ee1d5d
knopgm <knop.gm@gmail.com>
2023-10-26 22:52:37 +0200

    fix: Update carousel behaviour of modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,86 +1,83 @@
+//------------------------------------------------------
+// OPENING MODAL:
+
 // Get the modal -> id of the modal structure on modal.html
-const modal = document.getElementById("myModal");
+const modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-const openModal = document.querySelectorAll("img");
-const openImgModal = modal.querySelector("img");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const openModal = document.querySelectorAll('img');
+const openImgModal = modal.querySelector('img');
 
 // Create an array of images for the circle motion on arrow buttons
-console.log({ openModal });
-const projectImages = [];
+const projectImageSrcs = [];
 
 // When the user clicks on the button(img), open the modal
 openModal.forEach((item) => {
   const target = item.src;
-  if (target !== "") {
-    projectImages.push(target);
+  if (target !== '') {
+    projectImageSrcs.push(target);
   }
 
-  item.addEventListener("click", (e) => {
+  item.addEventListener('click', (e) => {
     const target = e.target.src;
-    console.log({ target });
-    modal.style.display = "block";
-    openImgModal.setAttribute("src", target);
+    modal.style.display = 'block';
+    openImgModal.setAttribute('src', target);
   });
 });
+//------------------------------------------------------
 
-console.log({ projectImages });
+//------------------------------------------------------
+// CLOSING MODAL:
+// Get the <span> element that closes the modal
+const spanClose = document.getElementsByClassName(
+  'material-symbols-outlined--close'
+)[0];
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+spanClose.onclick = function () {
+  modal.style.display = 'none';
 };
+//------------------------------------------------------
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+//------------------------------------------------------
+// CAROUSEL OF IMAGES:
+// Get the arrow buttons
+const arrows = [];
 
-//Get the arrow buttons
-const arrows = document.getElementsByClassName("arrow");
-console.log({ arrows });
+const next = document.querySelector('.material-symbols-outlined--arrow_right');
+arrows.push(next);
 
-const next = document.querySelectorAll("arrow right");
-console.log({ next });
-
-const previous = document.querySelectorAll("arrow left");
-console.log({ previous });
+const previous = document.querySelectorAll(
+  '.material-symbols-outlined--arrow_left'
+);
+arrows.push(...previous);
 
 // When the user clicks on the arrow buttons, the image will change
-[...arrows].forEach((item) => {
-  item.addEventListener("click", (e) => {
+arrows.forEach((item) => {
+  item.addEventListener('click', (e) => {
     const target = e.target;
-    console.log({ target });
     const imageSrc = openImgModal.src;
 
-    let currentIndex = projectImages.indexOf(imageSrc);
-    console.log({ currentIndex });
+    let currentIndex = projectImageSrcs.indexOf(imageSrc);
 
-    if (target === arrow_right) {
-      console.log("right");
-      if (currentIndex === projectImages.length - 1) {
+    if (target === next) {
+      if (currentIndex === projectImageSrcs.length - 1) {
         currentIndex = 0;
       } else {
         currentIndex = currentIndex + 1;
       }
 
-      console.log({ currentIndex });
-      openImgModal.setAttribute("src", projectImages[currentIndex]);
+      openImgModal.setAttribute('src', projectImageSrcs[currentIndex]);
     }
 
-    if (target === arrow_left) {
-      console.log("left");
+    if (target === previous[0] || target === previous[1]) {
       if (currentIndex === 0) {
-        currentIndex = projectImages.length - 1;
+        currentIndex = projectImageSrcs.length - 1;
       } else {
         currentIndex = currentIndex - 1;
       }
-      console.log({ currentIndex });
-      openImgModal.setAttribute("src", projectImages[currentIndex]);
+
+      openImgModal.setAttribute('src', projectImageSrcs[currentIndex]);
     }
   });
 });
+//------------------------------------------------------
